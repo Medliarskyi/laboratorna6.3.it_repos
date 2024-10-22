80ccafb6ca00f7c28790d2dd1a16cdeb593abbc0
Medliarskyi <med.uras111@gmail.com>
2024-10-22 21:54:47 +0300

    first master commit

---
+++ UnitTest63it/UnitTest63it.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab63it/lab63it.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest63it
+{
+	TEST_CLASS(UnitTest63it)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int arr[] = { 3, -1, 5, -10, 7, 0, -2, 8 };
+			int size = sizeof(arr) / sizeof(arr[0]);
+			int expectedSum = 23; 
+
+			Assert::AreEqual(expectedSum, sumPositiveElements(arr, size));
+		}
+	};
+}
---
+++ lab63it/lab63it.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <iomanip> // Для форматованого виведення
+
+using namespace std;
+
+// Функція для виведення масиву
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << setw(5) << arr[i]; // формат виведення з відступом 5 символів
+    }
+    cout << endl;
+}
+
+// Функція для обчислення суми додатних елементів (ітераційно)
+int sumPositiveElements(int arr[], int size) {
+    int sum = 0;
+    for (int i = 0; i < size; i++) {
+        if (arr[i] > 0) {
+            sum += arr[i];
+        }
+    }
+    return sum;
+}
+
+int main() {
+    // Створимо масив
+    int arr[] = { 3, -1, 5, -10, 7, 0, -2, 8 };
+    int size = sizeof(arr) / sizeof(arr[0]); // обчислюємо розмір масиву
+
+    // Виводимо масив
+    cout << "Array: ";
+    printArray(arr, size);
+
+    // Обчислюємо суму додатних елементів
+    int sum = sumPositiveElements(arr, size);
+
+    // Виводимо результат
+    cout << "The sum of positive elements: " << sum << endl;
+
+    return 0;
+}
